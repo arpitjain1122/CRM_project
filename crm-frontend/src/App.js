f34c1bc2270878bc9a5204627f8c234709128f64
arpit jain <jain.arpit@northeastern.edu>
2024-03-16 19:13:14 -0400

    layout is added

--- crm-frontend/src/App.js
+++ crm-frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { Button } from 'react-bootstrap';
-import { Entry } from './pages/entry/Entry.page';
+import { Button } from "react-bootstrap";
+import { Entry } from "./pages/entry/Entry.page";
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div>
-      <Entry/>     
+      {/* <Entry/>      */}
+      <DefaultLayout>
+        {/*passing props */}
+        send page content
+      </DefaultLayout>
     </div>
   );
 }
